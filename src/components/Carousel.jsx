2b142f061223e515d409778a9e99cfb7f91aa75e
MOJAN <sjs732@naver.com>
2024-05-26 17:58:02 +0900

    Feat: banner 추가, Carousel 컴포넌트 추가

---
+++ src/components/Carousel.jsx
@@ -0,0 +1,52 @@
+import { useEffect, useState } from "react"
+import styled from "styled-components"
+
+const CarouselWrapper = styled.div`
+    width: min(100%, 500px);
+    overflow-x: hidden;
+`
+
+const CarouselMovingContainer = styled.div`
+    display: flex;
+    width: min(100%, 500px);
+    background-size: cover;
+    transform: translate(max(${(props) => props.index * -100}vw, ${(props) => props.index * -500}px));
+    transition-duration: 1s;
+`
+const CarouselItemImage = styled.img`
+    width: min(100vw, 500px);
+`
+
+const Carousel = (props) => {
+    const [CarouselIndex, setCarouselIndex] = useState(0);
+    const moveCarousel = () => {
+        if(CarouselIndex >= props.items.length-1)
+            setCarouselIndex(0);
+        else
+            setCarouselIndex(CarouselIndex + 1);
+    }
+
+    useEffect(() => {
+        const timer = setInterval(() => {
+            moveCarousel();
+        }, 7000);
+        return () => clearInterval(timer);
+    }, [CarouselIndex]);
+
+    return (
+        <CarouselWrapper>
+            <CarouselMovingContainer index={CarouselIndex}>
+                {props.items.map((item, index) => <CarouselItem key={index} item={item}/>)}
+            </CarouselMovingContainer>
+        </CarouselWrapper>
+    )
+} 
+
+
+const CarouselItem = (props) => {
+    return (
+        <CarouselItemImage src={props.item.src} />
+    )
+}
+
+export default Carousel;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,29 @@
+import styled from "styled-components";
+import Carousel from "../components/Carousel";
+
+const BannerDataList = [
+    {
+        src: "../src/assets/banners/banner1.png"
+    },
+    {
+        src: "../src/assets/banners/banner2.png"
+    },
+    {
+        src: "../src/assets/banners/banner3.png"
+    }
+]
+
+const HomeWrapper = styled.div`
+    display: flex;
+    flex-direction: column;
+`
+
 const Home = () => {
     return(
-        <>
-            This is Home
-        </>
+        <HomeWrapper>
+            <Carousel items={BannerDataList} />
+            Hello, Modak!
+        </HomeWrapper>
     )
 }
 
