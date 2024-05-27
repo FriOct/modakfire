f450aef52caef932c334a1017a8d6dc014f09990
jhssong <jhssong02@gmail.com>
2024-05-28 04:19:08 +0900

    feat: Add firebase

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,9 +1,55 @@
-const Login = () => {
-    return (
-        <>
-            This is Login
-        </>
-    )
-}
+import styled from "styled-components";
+import googleLogo from "../assets/images/google_logo.png";
+import LightTitle from "../components/Text/LightTitle";
+import { googleLogin } from "../utils/firebase";
 
-export default Login
+const PageWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: min(100vw, 500px);
+`;
+
+const ButtonWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin: 0 auto;
+  width: min(65vw, 350px);
+  height: min(11vw, 55px);
+  border-radius: min(2vw, 10px);
+  box-shadow: 0px 0px 8px ${({ theme }) => theme.color.shadow};
+  img {
+    width: min(5vw, 25px);
+    background-size: cover;
+  }
+`;
+
+const ButtonTextWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  gap: min(2vw, 10px);
+`;
+
+const GoogleLoginBtn = () => {
+  return (
+    <ButtonWrapper>
+      <ButtonTextWrapper onClick={googleLogin}>
+        <img src={googleLogo} />
+        <LightTitle>구글로 로그인하기</LightTitle>
+      </ButtonTextWrapper>
+    </ButtonWrapper>
+  );
+};
+
+const Login = () => {
+  return (
+    <PageWrapper>
+      This is Login
+      <GoogleLoginBtn />
+    </PageWrapper>
+  );
+};
+
+export default Login;
---
+++ src/utils/firebase.js
@@ -0,0 +1,53 @@
+import { initializeApp } from "firebase/app";
+import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID,
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+const provider = new GoogleAuthProvider();
+const auth = getAuth();
+
+function googleLogin() {
+  signInWithPopup(auth, provider)
+    .then((result) => {
+      // This gives you a Google Access Token. You can use it to access the Google API.
+      const credential = GoogleAuthProvider.credentialFromResult(result);
+      const token = credential.accessToken;
+      // The signed-in user info.
+      const user = result.user;
+      // IdP data available using getAdditionalUserInfo(result)
+      // ...
+    })
+    .catch((error) => {
+      // Handle Errors here.
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      // The email of the user's account used.
+      const email = error.customData.email;
+      // The AuthCredential type that was used.
+      const credential = GoogleAuthProvider.credentialFromError(error);
+      // ...
+    });
+}
+
+function googleLogout() {
+  signOut(auth)
+    .then(() => {
+      // Sign-out successful.
+    })
+    .catch((error) => {
+      // An error happened.
+    });
+}
+
+export { googleLogin, googleLogout };
