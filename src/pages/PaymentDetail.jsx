50a2a8abad97f50945ed2a39b04be4968936e513
pobi <researcherdragon@gmail.com>
2024-05-27 17:06:59 +0900

    feat : paymentdetail page 파일 생성

---
+++ src/pages/PaymentDetail.jsx
@@ -0,0 +1,23 @@
+import styled from "styled-components"
+
+
+const Container = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    padding: 0;
+    width: 100%;
+    height: 100vh;
+    background: #ffffff;
+`;
+
+
+
+const PaymentDetail = () => {
+  return (
+    <div>PaymentDetail</div>
+  )
+}
+
+export default PaymentDetail
