bd2ddc98c4fb721fbfa47db1d675bfa7fe657a9e
jhssong <jhssong02@gmail.com>
2024-05-28 19:36:12 +0900

    feat: Add Googlelogin

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import Logo from "../assets/logo.png";
 import googleLogo from "../assets/images/google_logo.png";
 import LightTitle from "../components/Text/LightTitle";
 import { googleLogin } from "../utils/firebase";
@@ -7,7 +8,13 @@ const PageWrapper = styled.div`
   display: flex;
   flex-direction: column;
   width: min(100vw, 500px);
+  height: 100vh;
+  justify-content: center;
+  align-items: center;
+  gap: min(2vw, 10px);
 `;
+
+const TitleText = styled.div``;
 
 const ButtonWrapper = styled.div`
   display: flex;
@@ -24,7 +31,7 @@ const ButtonWrapper = styled.div`
   }
 `;
 
-const ButtonTextWrapper = styled.div`
+const ButtonContentWrapper = styled.div`
   display: flex;
   flex-direction: row;
   align-items: center;
@@ -35,19 +42,52 @@ const ButtonTextWrapper = styled.div`
 const GoogleLoginBtn = () => {
   return (
     <ButtonWrapper>
-      <ButtonTextWrapper onClick={googleLogin}>
+      <ButtonContentWrapper onClick={googleLogin}>
         <img src={googleLogo} />
         <LightTitle>구글로 로그인하기</LightTitle>
-      </ButtonTextWrapper>
+      </ButtonContentWrapper>
     </ButtonWrapper>
   );
 };
 
+const MainTitleText = styled.h1`
+  font-size: ${({ theme }) => theme.fontSize.xlarge};
+  font-weight: 900;
+  color: ${({ theme }) => theme.fontColor.brown};
+`;
+
+const SubTitleText = styled.p`
+  font-size: ${({ theme }) => theme.fontSize.medium};
+  font-weight: normal;
+  color: ${({ theme }) => theme.fontColor.brown};
+`;
+
+const LogoImage = styled.img`
+  width: min(34vw, 170px);
+  height: min(39vw, 195px);
+`;
+
+const Seperator = styled.div`
+  width: min(100vw, 500px);
+  height: min(16vw, 60px);
+`;
+
+const AnonymousLoginText = styled.p`
+  font-size: ${({ theme }) => theme.fontSize.base};
+  font-weight: normal;
+  color: ${({ theme }) => theme.fontColor.gray}; // TODO style 지정 안됨
+  color: #b5b5b5;
+`;
+
 const Login = () => {
   return (
     <PageWrapper>
-      This is Login
+      <LogoImage src={Logo} />
+      <MainTitleText>모닥불</MainTitleText>
+      <SubTitleText>지역사회 기부 플랫폼</SubTitleText>
+      <Seperator />
       <GoogleLoginBtn />
+      <AnonymousLoginText>로그인 없이 계속하기</AnonymousLoginText>
     </PageWrapper>
   );
 };
--- src/styles/theme.js
+++ src/styles/theme.js
@@ -1,31 +1,32 @@
 const fontSize = {
-    large: "min(6vw, 30px)",
-    medium: "min(4vw, 20px)",
-    base: "min(3vw, 15px)"
-}
+  xlarge: "min(8vw, 40px)",
+  large: "min(6vw, 30px)",
+  medium: "min(4vw, 20px)",
+  base: "min(3vw, 15px)",
+};
 
 const fontColor = {
-    brown: "#721B00",
-    primary: "#000000",
-    secondary: "#888888"
-}
+  brown: "#721B00",
+  primary: "#000000",
+  secondary: "#888888",
+};
 
 const color = {
-    bg: "#FFFFFF",
-    primary: "#FF8A3D",
-    secondary: "#FFF2D7",
-    black: "#000000",
-    lightgray: "#D7D7D7",
-    gray: "#B5B5B5",
-    drakgray: "#AAAAAA",
-    shadow: "#00000040",
-    highlight: "#FF8B3D80"
-}
- 
+  bg: "#FFFFFF",
+  primary: "#FF8A3D",
+  secondary: "#FFF2D7",
+  black: "#000000",
+  lightgray: "#D7D7D7",
+  gray: "#B5B5B5",
+  drakgray: "#AAAAAA",
+  shadow: "#00000040",
+  highlight: "#FF8B3D80",
+};
+
 const theme = {
-    fontSize,
-    fontColor,
-    color
-}
+  fontSize,
+  fontColor,
+  color,
+};
 
 export default theme;
