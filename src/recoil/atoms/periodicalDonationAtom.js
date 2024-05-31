ab274a5b0771145ac26a4379bb67c01174c186fb
pobi <researcherdragon@gmail.com>
2024-05-31 10:56:54 +0900

    test:Test를 위한 dummy데이터 추가

--- src/recoil/atoms/periodicalDonationAtom.js
+++ src/recoil/atoms/periodicalDonationAtom.js
@@ -4,10 +4,10 @@ export const periodicalDonationState = atom({
     key: "periodicalDonationState",
     default: [{
         periodical_donation_id: 0,
-        center_name: "센터",
-        start_date: null,
-        end_date: null,
-        donation_date: 0,
-        amount: 13,
+        center_name: "천광 보육원",
+        start_date: new Date(),
+        end_date: new Date(),
+        donation_date: 22,
+        amount: 200000,
     }],
 });
